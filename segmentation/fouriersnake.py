3b3d00eaf1f368645da6f77ef7b372fe11bc0372
Thanasi Athanassiadis <thanasi@uchicago.edu>
2012-02-16 13:25:22 -0600

    defined fourier snake cost

--- segmentation/fouriersnake.py
+++ segmentation/fouriersnake.py
@@ -3,6 +3,14 @@ import numpy as np
 import fourierd as fd
 import segmentation as seg
 
-def cost(im, contour, sigma = 1):
-    H = 0
-    for x in
+#cost heuristic for the fourier snake
+def cost(im, chain8, sigma = 1):
+    edge = seg.decode_chain8(chain8)
+    region = seg.flood_fill(edge)
+
+    m_in = (region * im).mean()
+    m_out =((1-region) * im).mean()
+    
+    H = ((im - m_in)**2 + (im - m_out)**2) / (2 * sigma**2)).sum()
+    
+    return H
--- segmentation/segmentation.py
+++ segmentation/segmentation.py
@@ -172,6 +172,71 @@ def path2coords(path):
     
     return np.array(coords)
     
+# get the optimal thresholding level for an n-dim image
+# from Otsu Threshold Selection Method paper
+def otsu_level(im, plot=True):
+    if im.std() < 10e-10:
+        print 'otsu_level: Uniform Image, no optimal threshold'
+        return -1
+    pdf, bins = np.histogram(im,bins=256,density=True)
+    bins = bins[1:]
+    width = bins[1]-bins[0]
+    
+    mt = (pdf * bins).sum()
+    st2 = ((bins - mt)**2 * pdf).sum()
+    
+    sb2 = []
+    
+    for k in range(1,len(bins)):
+        w0 = pdf[:k].sum() * width
+        w1 = pdf[k:].sum() * width
+        m0 = (bins * pdf)[:k].sum() / w0
+        m1 = (bins * pdf)[k:].sum() / w1
+        
+        sb2.append(w0 * w1 * (m1 - m0)**2)
+    
+    sb2 = np.array(sb2)
+    eta = sb2 / st2
+    
+    kk = bins[eta==eta.max()][0]
+    if plot:
+        pl.plot(bins+width/2,pdf)
+        pl.axvline(kk)
+        pl.show()
+   
+    return kk
+    
+# fill the interior of a region (including the boundary)
+# using a cue
+# input should be binary edge of a region with no holes
+
+def flood_fill(edge, i=None, j=None):
+    region = edge.copy()
+    if i==None or j==None:
+        i,j = CoM(edge)
+    
+    q = [(i,j)]
+    
+    while len(q) != 0:
+        i,j = q.pop(0)
+        if region[i,j]==0:
+            region[i,j] = 1
+            q.append((i-1,j))
+            q.append((i+1,j))
+            q.append((i,j+1))
+            q.append((i,j-1))
+    return region
+
+# perform 2-dimensional center of mass calculation
+def CoM(im):
+    center = np.zeros(2)
+    shape = np.array(im.shape)
+    for i in range(shape[0]):
+        for j in range(shape[1]):
+            center += im[i,j]*np.array([i,j])
+    center /= 1.0*len(np.nonzero(im)[0])
+    return np.floor(center)
+    
 # write the chain-code
 def write_chain(fn, chain):
     with open(fn,'w') as of:
